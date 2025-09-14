87c2b7344e4e536b22d4d5f8cfd80faef3716dbb
Harshit Sharma <harshitsharma5810@gmail.com>
2025-09-14 13:47:30 +0530

    Create weatherapp.py

---
+++ weatherapp.py
@@ -0,0 +1,41 @@
+import requests
+
+API_KEY = "de344ea2bc73b33e12d215c4b748b083"
+ZIP_CODE = '110001'
+COUNTRY_CODE = 'IN'
+
+URL = 'http://api.openweathermap.org/geo/1.0/zip?zip={},{}&appid={}'.format(ZIP_CODE,COUNTRY_CODE,API_KEY)
+
+response = requests.get(URL)
+data = response.json()
+lat = data['lat']
+lon = data['lon']    
+
+def Get_Weather(City_Name):
+    Main_URL = 'https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}'.format(lat, lon, API_KEY)
+    Response = requests.get(Main_URL)
+    Data = Response.json()
+
+    if Data['cod'] == 200:
+        Weather_Info = {
+            'City': data['name'],
+            'Latitude': (lat),
+            'longitude': (lon),
+            'Temperature(Celsius)': Data['main']['temp'],
+            'Feels Like(Celsius)': Data['main']['feels_like'],
+            'Description': Data['weather'][0]['description'],
+            'Humidity (%)': Data['main']['humidity'],
+            'Wind Speed (m/s)': Data ['wind']['speed'],
+        }
+        return Weather_Info
+    else:
+        print("Error")
+
+def main():
+    City_Name = input("Enter City Name: ")
+    while(City_Name==""):
+        City_Name = input("Enter City Name: ")
+    Weather_Data = Get_Weather(City_Name)
+    print(Weather_Data)
+
+main()
