eee183fee5853e50ef4da17fe9848dcecadd693f
Harshit Sharma <harshitsharma5810@gmail.com>
2025-10-01 15:26:41 +0530

    Update weatherapp.py

--- weatherapp.py
+++ weatherapp.py
@@ -7,29 +7,35 @@ COUNTRY_CODE = 'IN'
 URL = 'http://api.openweathermap.org/geo/1.0/zip?zip={},{}&appid={}'.format(ZIP_CODE,COUNTRY_CODE,API_KEY)
 
 response = requests.get(URL)
-data = response.json()
-lat = data['lat']
-lon = data['lon']    
+Location_Data = response.json()
+lat = Location_Data['lat']
+lon = Location_Data['lon']    
 
 def Get_Weather():
     Main_URL = 'https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}'.format(lat, lon, API_KEY)
     Response = requests.get(Main_URL)
-    Data = Response.json()
+    Weather_Data = Response.json()
 
-    if Data['cod'] == 200:
+    if Weather_Data['cod'] == 200:
         Weather_Info = {
-            'City': data['name'],
+            'City': Location_Data['name'],
             'Latitude': (lat),
-            'longitude': (lon),
-            'Temperature(Celsius)': Data['main']['temp'],
-            'Feels Like(Celsius)': Data['main']['feels_like'],
-            'Description': Data['weather'][0]['description'],
-            'Humidity (%)': Data['main']['humidity'],
-            'Wind Speed (m/s)': Data ['wind']['speed'],
+            'Longitude': (lon),
+            'Temperature (Celsius)': round(Weather_Data['main']['temp'] - 273.15, 2),
+            'Feels Like (Celsius)': round(Weather_Data['main']['feels_like'] - 273.15, 2),
+            'Description': Weather_Data['weather'][0]['description'],
+            'Humidity (%)': Weather_Data['main']['humidity'],
+            'Wind Speed (m/s)': Weather_Data ['wind']['speed'],
         }
         return Weather_Info
     else:
-        print("Error")
+        print("I Think Something Is Wrong With Your API")
+
+def main():
+    Weather_Data = Get_Weather()
+    print(Weather_Data)
+
+main()
 
 def main():
     Weather_Data = Get_Weather()
